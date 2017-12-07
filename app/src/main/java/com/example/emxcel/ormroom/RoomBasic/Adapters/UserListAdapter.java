66c3b2677f756157b6d83090dd225e36ba071206
Jay Parikh <jay.parikh@emxcelsolutions.com>
2017-12-07 19:24:22 +0530

    recycle list done

---
+++ app/src/main/java/com/example/emxcel/ormroom/RoomBasic/Adapters/UserListAdapter.java
@@ -0,0 +1,66 @@
+package com.example.emxcel.ormroom.RoomBasic.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.example.emxcel.ormroom.R;
+import com.example.emxcel.ormroom.RoomBasic.Tables.UserInfo;
+
+import java.util.List;
+
+import io.reactivex.Completable;
+
+/**
+ * Created by emxcel on 7/12/17.
+ */
+
+public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHoler> {
+    private Context context;
+    private List<UserInfo> userList;
+    
+    public UserListAdapter(Context context, List<UserInfo> userList) {
+        this.context = context;
+        this.userList = userList;
+    }
+
+    @Override
+    public ViewHoler onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_user_list, parent, false);
+
+
+        return new ViewHoler(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHoler holder, int position) {
+        UserInfo userInfo = userList.get(position);
+        holder.tvUserId.setText(String.valueOf(userInfo.getId()));
+        holder.tvUserName.setText(userInfo.getName());
+        holder.tvUserAge.setText(String.valueOf(userInfo.getAge()));
+        holder.tvUserPremium.setText(String.valueOf(userInfo.isPremium()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+
+    public class ViewHoler extends RecyclerView.ViewHolder {
+        public TextView tvUserId, tvUserName, tvUserAge, tvUserPremium;
+
+        public ViewHoler(View itemView) {
+            super(itemView);
+            tvUserId = itemView.findViewById(R.id.tv_user_id);
+            tvUserName = itemView.findViewById(R.id.tv_user_name);
+            tvUserAge = itemView.findViewById(R.id.tv_user_age);
+            tvUserPremium = itemView.findViewById(R.id.tv_user_premium);
+        }
+    }
+}
